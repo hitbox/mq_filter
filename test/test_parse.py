52476a28b4c041d339d623aeb561b0aca98d658e
Carl L. Harris <Carl.Harris@airborneglobal.com>
2026-02-19 13:11:49 -0500

    Test for RFH2 header function

--- test/test_parse.py
+++ test/test_parse.py
@@ -3,6 +3,7 @@ import unittest
 from mq_filter.parse import apis
 from mq_filter.parse import detect
 from mq_filter.parse import dlnk
+from mq_filter.parse import extract_payload_from_mq
 from mq_filter.parse import flight_plan
 from mq_filter.parse import simple
 
@@ -32,3 +33,11 @@ class TestParse(unittest.TestCase):
     def test_parse_mv(self):
         self.check_file('test/MVA ATN.txt', simple, 'ATN')
         self.check_file('test/MVT GB.txt', simple, 'GB')
+
+class TestExtract(unittest.TestCase):
+
+    def test_extract_payload(self):
+        # Test weird RFH messages get parsed to extract the plain text message.
+        message = b"RFH \x00\x00\x00\x02\x00\x00\x01P\x00\x00\x01\x11\x00\x00\x04\xb8MQSTR   \x00\x00\x00\x00\x00\x00\x04\xb8\x00\x00\x00 <mcd><Msd>jms_text</Msd></mcd>  \x00\x00\x00X<jms><Dst>queue:///ISB.STAGSMX.SND.ARINC</Dst><Tms>1770591522997</Tms><Dlv>2</Dlv></jms>\x00\x00\x00\xa8<usr><breadcrumbId>125B06B71564E10-0000000000000F4C</breadcrumbId><CamelJmsDeliveryMode dt='i4'>2</CamelJmsDeliveryMode><IOCC_TLX_ID>MRS144594901</IOCC_TLX_ID></usr>   \r\n\x01QU ASIKORR\r\n.ATSGOPS 082258\r\n\x02MVA\r\nATN530/08.N751CX.ANU\r\nAD2229/2258 EA0555 ASI\r\n\x03"
+        payload = extract_payload_from_mq(message)
+        self.assertEqual(payload, '\r\n\x01QU ASIKORR\r\n.ATSGOPS 082258\r\n\x02MVA\r\nATN530/08.N751CX.ANU\r\nAD2229/2258 EA0555 ASI\r\n\x03')
